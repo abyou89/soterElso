7fbaae0227143c1508b880537afc0ba53a7b3e04
younes.abdalla <younes.abdalla@gmail.com>
2024-05-25 09:35:32 +0200

    project modositva

--- src/gitossz/GitOssz.java
+++ src/gitossz/GitOssz.java
@@ -1,20 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package gitossz;
 
-/**
- *
- * @author valak
- */
-public class GitOssz {
 
-    /**
-     * @param args the command line arguments
-     */
+public class GitOssz {
+    
+    public static int[] szamok;
+
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int[] tomb = feltoltes();
+        tombKiir(tomb);
+    
+    }
+
+    private static int[] feltoltes() {
+        szamok = new int[5];
+        for (int i = 0; i < szamok.length; i++) {
+            szamok[i] = (int)(Math.random()*6)+1;
+        }
+    return szamok;
+    }
+
+    private static void tombKiir(int[] tomb) {
+        for (int i = 0; i < tomb.length; i++) {
+            System.out.printf("%d", tomb[i]);
+        }
     }
     
 }
